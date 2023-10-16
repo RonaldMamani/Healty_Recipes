8b72d462f6a79b8a54128e1fd666c5cc6876dd6a
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-10-16 16:49:11 -0300

    Consumindo API e gerando single page de cada receita

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { SearchComponent } from './components/pages/search/search.component';
+import { SingleRecipesComponent } from './components/pages/single-recipes/single-recipes.component';
 
 const routes: Routes = [
   {path: '' , component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'search', component: SearchComponent},
+  {path: 'recipe', component: SingleRecipesComponent}
 ];
 
 @NgModule({
---
+++ src/app/services/single-recipe/single-recipe.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SingleRecipeService {
+
+  constructor(private http: HttpClient) {}
+
+  takeRecipe(id: number) {
+    const apiUrl = `https://api.spoonacular.com/recipes/${id}/information?apiKey=22489109b6a84f75b2f085db20b9bdef`;
+
+    return this.http.get(apiUrl);
+  }
+}
