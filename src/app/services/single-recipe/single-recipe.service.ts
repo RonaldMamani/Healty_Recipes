8101f72754a796875ac08e38e6d543c93117a3a8
Ronaldo Arley <ronaldmamani140@gmail.com>
2023-10-16 16:59:31 -0300

    Adicionando funcionalidade de Receitas Similares e navegando entre elas

--- src/app/services/single-recipe/single-recipe.service.ts
+++ src/app/services/single-recipe/single-recipe.service.ts
@@ -13,4 +13,10 @@ export class SingleRecipeService {
 
     return this.http.get(apiUrl);
   }
+
+  takeSimilarRecipe(id: number) {
+    const apiUrl = `https://api.spoonacular.com/recipes/${id}/similar?apiKey=22489109b6a84f75b2f085db20b9bdef`;
+  
+    return this.http.get(apiUrl);
+  }
 }
